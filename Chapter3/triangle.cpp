e887c3a354ae80b7bff4318c75e8d83a181ff576
6carol6 <mszxw999@gmail.com>
2016-11-08 11:15:30 +0800

    triangle faild - sad

---
+++ Chapter3/triangle.cpp
@@ -0,0 +1,134 @@
+#include "d3dUtility.h"
+IDirect3DDevice9 *Device = 0;
+IDirect3DVertexBuffer9* VB = 0;
+IDirect3DIndexBuffer9* IB = 0;
+
+const int Width = 800;
+const int Height = 600;
+
+struct Vertex {
+	Vertex() {}
+	Vertex(float x, float y, float z) {
+		_x = x; _y = y; _z = z;
+	}
+	float _x, _y, _z;
+	static const DWORD FVF;
+};
+
+const DWORD Vertex::FVF = D3DFVF_XYZ;
+
+bool Setup() {
+	Device->CreateVertexBuffer(
+		3 * sizeof(Vertex),
+		D3DUSAGE_WRITEONLY,
+		Vertex::FVF,
+		D3DPOOL_MANAGED,
+		&VB,
+		0);
+	Device->CreateIndexBuffer(
+		3 * sizeof(WORD),
+		D3DUSAGE_WRITEONLY,
+		D3DFMT_INDEX16,
+		D3DPOOL_MANAGED,
+		&IB,
+		0);
+
+	// Fill the buffers with the triangle data.
+	Vertex* vertices;
+	VB->Lock(0, 0, (void**)&vertices, 0);
+
+	//vertices of a triangle
+	vertices[0] = Vertex(0.0f, 1.0f, 0.0f);
+	vertices[1] = Vertex(1.0f, 0.0f, 0.0f);
+	vertices[2] = Vertex(-1.0f, 0.0f, 0.0f);
+
+	VB->Unlock();
+
+	DWORD* indices = 0;
+	IB->Lock(0, 0, (void**)&indices, 0);
+
+	indices[0] = 0; indices[1] = 1; indices[2] = 2;
+
+	IB->Unlock();
+
+	// Position and aim the camera
+	D3DXVECTOR3 position(0.0f, 0.0f, -5.0f);
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+
+	D3DXMATRIX V;
+	D3DXMatrixLookAtLH(&V, &position, &target, &up);
+
+	Device->SetTransform(D3DTS_VIEW, &V);
+
+	// Set the projection matrix.
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(
+		&proj,
+		D3DX_PI * 0.5f, // 90 - degree
+		(float)Width / (float)Height,
+		1.0f,
+		1000.0f);
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+
+	// Switch to wireframe mode.
+	Device->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+
+	return true;
+}
+
+bool Display(float timeDelta) {
+	if (Device) {
+		//draw the scene:
+		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+		Device->BeginScene();
+
+		Device->SetStreamSource(0, VB, 0, sizeof(Vertex));
+		Device->SetIndices(IB);
+		Device->SetFVF(Vertex::FVF);
+
+		// Draw triangle.
+		Device->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 3, 0, 1);
+
+		Device->EndScene();
+		Device->Present(0, 0, 0, 0);
+	}
+	return true;
+}
+
+void Cleanup() {
+	d3d::Release<IDirect3DVertexBuffer9*>(VB);
+	d3d::Release<IDirect3DIndexBuffer9*>(IB);
+}
+
+LRESULT CALLBACK d3d::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			::DestroyWindow(hwnd);
+		break;
+	}
+	return ::DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd) {
+	if (!d3d::InitD3D(hinstance, 800, 600, true, D3DDEVTYPE_HAL, &Device)) {
+		::MessageBox(0, L"InitD3D()-FAILED", 0, 0);
+		return 0;
+	}
+	if (!Setup()) {
+		::MessageBox(0, L"InitD3D()-FAILED", 0, 0);
+		return 0;
+	}
+
+	d3d::EnterMsgLoop(Display);
+
+	Cleanup();
+
+	Device->Release();
+	return 0;
+}
