2caa678f549ef88957efedd0a913986946c789b7
6carol6 <mszxw999@gmail.com>
2016-11-19 00:26:41 +0800

    colored triangle

---
+++ Chapter4/triangle.cpp
@@ -0,0 +1,129 @@
+
+#include "d3dUtility.h"
+IDirect3DDevice9 *Device = 0;
+IDirect3DVertexBuffer9* Triangle = 0;
+
+const int Width = 800;
+const int Height = 600;
+
+struct ColorVertex {
+	ColorVertex(){}
+	ColorVertex(float _x, float _y, float _z, D3DCOLOR _color) {
+		x = _x; y = _y; z = _z; color = _color;
+	}
+	float x, y, z;
+	D3DCOLOR color;
+	static const DWORD FVF;
+};
+
+const DWORD ColorVertex::FVF = D3DFVF_XYZ | D3DFVF_DIFFUSE;
+
+bool Setup() {
+	Device->CreateVertexBuffer(
+		3 * sizeof(ColorVertex),
+		D3DUSAGE_WRITEONLY,
+		ColorVertex::FVF,
+		D3DPOOL_MANAGED,
+		&Triangle,
+		0);
+	
+	// Fill the buffers with the triangle data.
+	ColorVertex* v;
+	Triangle->Lock(0, 0, (void**)&v, 0);
+
+	//vertices of a triangle
+	v[0] = ColorVertex(-1.0f, 0.0f, 2.0f, D3DCOLOR_XRGB(255, 0, 0));
+	v[1] = ColorVertex(0.0f, 1.0f, 2.0f, D3DCOLOR_XRGB(0, 255, 0));
+	v[2] = ColorVertex(1.0f, 0.0f, 2.0f, D3DCOLOR_XRGB(0, 0, 255));
+
+	Triangle->Unlock();
+	
+	// Position and aim the camera
+	D3DXVECTOR3 position(0.0f, 0.0f, -5.0f);
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+
+	D3DXMATRIX V;
+	D3DXMatrixLookAtLH(&V, &position, &target, &up);
+
+	Device->SetTransform(D3DTS_VIEW, &V);
+	
+	// Set the projection matrix.
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(
+		&proj,
+		D3DX_PI * 0.5f, // 90 - degree
+		(float)Width / (float)Height,
+		1.0f,
+		1000.0f);
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+
+	// Turn off the lighting.
+	Device->SetRenderState(D3DRS_LIGHTING, false);
+
+	return true;
+}
+
+bool Display(float timeDelta) {
+	if (Device) {
+		//draw the scene:
+		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+		Device->BeginScene();
+
+		Device->SetStreamSource(0, Triangle, 0, sizeof(ColorVertex));
+		Device->SetFVF(ColorVertex::FVF);
+
+		// Draw the triangle to the left with flat shading
+		D3DXMATRIX WorldMatrix;
+		D3DXMatrixTranslation(&WorldMatrix, -1.25f, 0.0f, 0.0f);
+		Device->SetTransform(D3DTS_WORLD, &WorldMatrix);
+		Device->SetRenderState(D3DRS_SHADEMODE, D3DSHADE_FLAT);
+		Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+
+		// Draw the triangle to the right with gouraud shading
+		D3DXMatrixTranslation(&WorldMatrix, 1.25f, 0.0f, 0.0f);
+		Device->SetTransform(D3DTS_WORLD, &WorldMatrix);
+		Device->SetRenderState(D3DRS_SHADEMODE, D3DSHADE_GOURAUD);
+		Device->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+
+		Device->EndScene();
+		Device->Present(0, 0, 0, 0);
+	}
+	return true;
+}
+
+void Cleanup() {
+	d3d::Release<IDirect3DVertexBuffer9*>(Triangle);
+}
+
+LRESULT CALLBACK d3d::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			::DestroyWindow(hwnd);
+		break;
+	}
+	return ::DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd) {
+	if (!d3d::InitD3D(hinstance, 800, 600, true, D3DDEVTYPE_HAL, &Device)) {
+		::MessageBox(0, L"InitD3D()-FAILED", 0, 0);
+		return 0;
+	}
+	if (!Setup()) {
+		::MessageBox(0, L"InitD3D()-FAILED", 0, 0);
+		return 0;
+	}
+
+	d3d::EnterMsgLoop(Display);
+
+	Cleanup();
+
+	Device->Release();
+	return 0;
+}
