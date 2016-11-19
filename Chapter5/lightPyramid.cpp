aad16708249592f65761a9ac0eba49b027d92389
6carol6 <mszxw999@gmail.com>
2016-11-19 20:07:48 +0800

    lightpyramid-untest

---
+++ Chapter5/lightPyramid.cpp
@@ -0,0 +1,147 @@
+#include "d3dUtility.h"
+
+IDirect3DDevice9* Device = 0;
+IDirect3DVertexBuffer9* Pyramid = 0;
+
+struct Vertex{
+    float x, y, z;
+    float _nx, _ny, _nz;
+    static const DWORD FVF;
+};
+const DWORD Vertex::FVF = D3DFVF_XYZ | D3DFVF_NORMAL;
+
+bool Setup(){
+    Device->SetRenderState(D3DRS_LIGHTING, true);
+
+    Device->CreateVertexBuffer(
+        12*sizeof(Vertex),
+        D3DUSAGE_WRITEONLY,
+        Vertex::FVF,
+        D3DPOOL_MANAGED,
+        &Pyramid,
+        0);
+
+    // Fill the vertex buffer with pyramid data
+    Vertex* v;
+    Pyramid->Lock(0, 0, (void**)&v, 0);
+
+    // front face
+    v[0] = Vertex(-1.0f, 0.0f, -1.0f, 0.0f, 0.707f, -0.707f);
+    v[1] = Vertex(0.0f, 1.0f, 0.0f, 0.0f, 0.707f, -0.707f);
+    v[2] = Vertex(1.0f, 0.0f, -1.0f, 0.0f, 0.707f, -0.707f);
+
+    // left face
+    v[3] = Vertex(-1.0f, 0.0f, 1.0f, -0.707f, 0.707f, 0.0f);
+    v[4] = Vertex(0.0f, 1.0f, 0.0f, -0.707f, 0.707f, 0.0f);
+    v[5] = Vertex(-1.0f, 0.0f, -1.0f, -0.707f, 0.707f, 0.0f);
+
+    // right face
+    v[6] = Vertex(1.0f, 0.0f, -1.0f, 0.707f, 0.707f, 0.0f);
+    v[7] = Vertex(0.0f, 1.0f, 0.0f, 0.707f, 0.707f, 0.0f);
+    v[8] = Vertex(1.0f, 0.0f, 1.0f, 0.707f, 0.707f, 0.0f);
+
+    // back face
+    v[9] = Vertex(1.0f, 0.0f, 1.0f, 0.0f, 0.707f, 0.707f);
+    v[10] = Vertex(0.0f, 1.0f, 0.0f, 0.0f, 0.707f, 0.707f);
+    v[11] = Vertex(-1.0f, 0.0f, 1.0f, 0.0f, 0.707f, 0.707f);
+
+    Pyramid->Unlock();
+
+    D3DMATERIAL9 mtrl;
+    mtrl.Ambient = d3d::WHITE;
+    mtrl.Diffuse = d3d::WHITE;
+    mtrl.Specular = d3d::WHITE;
+    mtrl.Emissive = d3d::BLACK;
+    mtrl.Power = 5.0f;
+
+    Device->SetMaterial(&mtrl);
+
+    D3DLIGHT9 dir;
+    ::ZeroMemory(&dir, sizeof(dir));
+    dir.Type = D3DLIGHT_DIRECTIONAL;
+    dir.Diffuse = d3d::WHITE;
+    dir.Specular = d3d::WHITE * 0.3f;
+    dir.Ambient = d3d::WHITE * 0.6f;
+    dir.Direction = D3DXVECTOR3(1.0f, 0.0f, 0.0f);
+
+    Device->SetLight(0, &dir);
+    Device->LightEnable(0, true);
+    Device->SetRenderState(D3DRS_NORMALIZENORMALS, true);
+    Device->SetRenderState(D3DRS_SPECULARENABLE, true);
+
+    // Position and aim the camera
+	D3DXVECTOR3 position(0.0f, 0.0f, -5.0f);
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+
+	D3DXMATRIX V;
+	D3DXMatrixLookAtLH(&V, &position, &target, &up);
+
+	Device->SetTransform(D3DTS_VIEW, &V);
+	
+	// Set the projection matrix.
+	D3DXMATRIX proj;
+	D3DXMatrixPerspectiveFovLH(
+		&proj,
+		D3DX_PI * 0.5f, // 90 - degree
+		(float)Width / (float)Height,
+		1.0f,
+		1000.0f);
+	Device->SetTransform(D3DTS_PROJECTION, &proj);
+
+    return true;
+}
+bool Display(float timeDelta) {
+	if (Device) {
+		//draw the scene:
+		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+		Device->BeginScene();
+
+		Device->SetStreamSource(0, VB, 0, sizeof(Vertex));
+		Device->SetIndices(IB);
+		Device->SetFVF(Vertex::FVF);
+
+		// Draw triangle.
+		Device->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 3, 0, 1);
+
+		Device->EndScene();
+		Device->Present(0, 0, 0, 0);
+	}
+	return true;
+}
+
+void Cleanup() {
+    d3d::Release<IDirect3DVertexBuffer9*>(Pyramid);
+}
+
+LRESULT CALLBACK d3d::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			::DestroyWindow(hwnd);
+		break;
+	}
+	return ::DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd) {
+	if (!d3d::InitD3D(hinstance, 800, 600, true, D3DDEVTYPE_HAL, &Device)) {
+		::MessageBox(0, L"InitD3D()-FAILED", 0, 0);
+		return 0;
+	}
+	if (!Setup()) {
+		::MessageBox(0, L"InitD3D()-FAILED", 0, 0);
+		return 0;
+	}
+
+	d3d::EnterMsgLoop(Display);
+
+	Cleanup();
+
+	Device->Release();
+	return 0;
+}
