156e631e8479c5b31bf8540fc5f69e35efc89a4d
6carol6 <mszxw999@gmail.com>
2016-11-22 22:52:56 +0800

    .h

--- Chapter5/d3dUtility.cpp
+++ Chapter5/d3dUtility.cpp
@@ -118,6 +118,11 @@ D3DLIGHT9 d3d::InitPointLight(D3DXVECTOR3* position, D3DXCOLOR* color) {
 	light.Diffuse = *color;
 	light.Specular = *color * 0.6f;
 	light.Position = *position;
+	light.Range = 1000.0f;
+	light.Falloff = 1.0f;
+	light.Attenuation0 = 1.0f;
+	light.Attenuation1 = 0.0f;
+	light.Attenuation2 = 0.0f;
 
 	return light;
 }
@@ -127,11 +132,18 @@ D3DLIGHT9 d3d::InitSpotLight(D3DXVECTOR3* position, D3DXVECTOR3* direction, D3DX
 	::ZeroMemory(&light, sizeof(light));
 
 	light.Type = D3DLIGHT_SPOT;
-	light.Ambient = *color * 0.4f;
+	light.Ambient = *color * 0.0f;
 	light.Diffuse = *color;
 	light.Specular = *color * 0.6f;
 	light.Position = *position;
 	light.Direction = *direction;
+	light.Range = 1000.0f;
+	light.Falloff = 1.0f;
+	light.Attenuation0 = 1.0f;
+	light.Attenuation1 = 0.0f;
+	light.Attenuation2 = 0.0f;
+	light.Theta = 0.4f;
+	light.Phi = 0.9f;
 
 	return light;
 }
